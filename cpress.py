17d6a4f895d472beec6eea631e9172c66f571f37
River Gillis <jrivergillis@gmail.com>
2016-09-09 10:17:25 -0500

    Add creation of compressed string

--- cpress.py
+++ cpress.py
@@ -1,4 +1,4 @@
-s = "aabbbbbbbbcdefffffffjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjiil"
+s = "aabbbbbbbbcdefffffffjjjjjjjjjjjjjj      jjjjj 1110000000101011111 jjjjjjjjjjjjjjjjjjjjjjjjjjiil"
 counter = []
 counter.append([]) # the first list will hold the character
 counter.append([]) # the second list will hold the number of times it apears in a row
@@ -7,16 +7,22 @@ char_index = -1
 counter[1].append(0)
 prevc = ''
 for i in s:
-    print("prevc: " + prevc + "i: " + i)
     if prevc != i:
         char_index += 1
-        print("char_index: " + str(char_index))
         counter[0].append(i)
-        print(counter[0])
         counter[1].append(0)
-        print(counter[1])
     counter[1][char_index] += 1
     prevc = i
 
+# create compressed string
+compressed = ""
+for i,c in enumerate(counter[0]):
+    # no point in compressing for two or less repeats
+    if counter[1][i] > 2:
+        compressed += (c + str(counter[1][i]))
+    else:
+        compressed += c*int(counter[1][i])
 print(counter)
 print(s)
+
+print(compressed)
